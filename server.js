00ac9faf4d0365a6ace6d3932cec5bd29fd4e8c8
abhirampjayan <abhirampjayan@outlook.com>
2022-12-29 22:28:39 +0530

    middleware created

--- server.js
+++ server.js
@@ -6,11 +6,21 @@ const fsPromises = require('fs').promises;
 const dotenv = require('dotenv');
 const express = require('express');
 
+const { logger } = require('./middleware/logEvents');
+
 dotenv.config({ path: __dirname + '/.env' });
 
 const PORT = process.env.PORT || 3500;
 
 const app = express();
+
+app.use(logger);
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('^/$|index(.html)?', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
